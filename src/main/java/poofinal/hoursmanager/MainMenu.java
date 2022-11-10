457bbd2f858ff4057f760688a2c64362b46645e2
João Lucas da Costa Fagundes <joaolucasdacf@gmail.com>
2022-11-09 23:34:47 -0300

    Mainmenu created

---
+++ src/main/java/poofinal/hoursmanager/MainMenu.java
@@ -0,0 +1,16 @@
+package poofinal.hoursmanager;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainMenu {
+    //Servidor
+    public TextField loginSIAPE;
+    public TextField passwordSIAPE;
+    public Button loginServidor;
+    //Aluno
+    public TextField loginCPF;
+    public TextField passwordCPF;
+    public Button loginAluno;
+
+}
