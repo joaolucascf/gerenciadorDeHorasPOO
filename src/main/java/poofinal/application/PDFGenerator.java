dc4a5eb1e412ea026a25dc5459ca1cd6a4397ae8
João Lucas da Costa Fagundes <joaolucasdacf@gmail.com>
2022-11-24 18:42:17 -0300

    Exportação do PDF de relatório está em perfeito funcionamento

---
+++ src/main/java/poofinal/application/PDFGenerator.java
@@ -0,0 +1,126 @@
+package poofinal.application;
+
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import poofinal.entities.Activities;
+import poofinal.entities.Student;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class PDFGenerator {
+
+    public static void pdfSaver(Path pdfPath, String pdfName) throws DocumentException, IOException {
+
+        File pdfOut = new File(pdfPath +"/"+pdfName+".pdf");
+
+        if(!Files.exists(pdfPath)) {
+            Files.createDirectories(pdfPath);
+        }
+        //instancia documento e cria sua saida
+        Document docPdf = new Document();
+        PdfWriter.getInstance(docPdf, new FileOutputStream(pdfOut));
+
+        //abre escrita no documento
+        docPdf.open();
+        docPdf = createReportPDF(docPdf);
+        docPdf.close();
+    }
+
+    private static Document createReportPDF(Document pdfReport) throws DocumentException, IOException {
+        Student student = Management.getStudentBuffer().get(Management.getKeyMatriculation());
+        List<Activities> acceptedActivities = student.getListActivities();
+
+        String isApproved;
+
+        //inicializa o tamanho do PDF e coloca um titulo no arquivo
+        pdfReport.setPageSize(PageSize.A4);
+        pdfReport.addTitle("Relatório de Admissão de Horas Complementares");
+
+        //adiciona o logo da ufpel
+        Image img = Image.getInstance("src/main/resources/files/img/UFPelShieldLogo.png");
+        img.scaleToFit(100,100);
+        pdfReport.add(img);
+
+        //adiciona o titulo no PDF
+        Font title = new Font(Font.FontFamily.HELVETICA, 28, Font.BOLD);
+        Paragraph titleLine = new Paragraph(String.format("Relatório de Admissão%nde Horas Complementares"), title);
+
+        titleLine.setIndentationLeft(img.getScaledWidth()+pdfReport.leftMargin());
+        titleLine.setSpacingBefore(img.getScaledHeight()*(-1));
+        pdfReport.add(titleLine);
+        titleLine.clear();
+
+        //adiciona dados do aluno
+        Paragraph writingLine = new Paragraph();
+        newBlankLine(writingLine, 3);
+        Font studentData = new Font(Font.FontFamily.HELVETICA, 12, Font.NORMAL);
+        writingLine.add(new Paragraph(String.format("Aluno:     %s%n", student.getName()), studentData));
+        writingLine.add(new Paragraph(String.format("Matrícula: %s%n", student.getMatriculation()), studentData));
+        writingLine.add(new Paragraph(String.format("E-mail:    %s%n", student.getEmail()), studentData));
+        writingLine.add(new Paragraph(String.format("Curso:     %s%n", student.getCourse().getDescription()), studentData));
+        newBlankLine(writingLine,3);
+        pdfReport.add(writingLine);
+        writingLine.clear();
+
+        //adiciona a tabela
+        newHoursTable(pdfReport, acceptedActivities);
+
+        return pdfReport;
+    }
+
+    private static void newHoursTable(Document pdfReport, List<Activities> activitiesPrintable) throws DocumentException {
+        float[] columnsWidth = {200, 50, 80};
+        int totalOfHoursApproved = 0;
+        PdfPTable hoursTable = new PdfPTable(3);
+        hoursTable.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hoursTable.setTotalWidth(columnsWidth);
+        PdfPCell column = new PdfPCell(new Phrase("ATIVIDADE"));
+        column.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hoursTable.addCell(column);
+        column = new PdfPCell(new Phrase("HORAS"));
+        column.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hoursTable.addCell(column);
+        column = new PdfPCell(new Phrase("SITUAÇÃO"));
+        column.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hoursTable.addCell(column);
+        hoursTable.setHeaderRows(1);
+        for(int i = 0; i<activitiesPrintable.size(); i++){
+            Activities printingActivitie = activitiesPrintable.get(i);
+            PdfPCell hourCell = new PdfPCell(new Phrase(printingActivitie.getDescription()));
+            hourCell.setHorizontalAlignment(Element.ALIGN_LEFT);
+            hoursTable.addCell(hourCell);
+            hourCell = new PdfPCell(new Phrase(printingActivitie.getHours()+" horas"));
+            hourCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            hoursTable.addCell(hourCell);
+            if(printingActivitie.getFlag()){
+                totalOfHoursApproved += Integer.parseInt(printingActivitie.getHours());
+                hourCell = new PdfPCell(new Phrase("APROVADO"));
+            }else{
+                hourCell = new PdfPCell(new Phrase("REPROVADO"));
+            }
+            hourCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+            hoursTable.addCell(hourCell);
+        }
+        PdfPCell totalCell = new PdfPCell(new Phrase("TOTAL DE HORAS ADQUIRIDAS"));
+        hoursTable.addCell(totalCell);
+        totalCell = new PdfPCell(new Phrase(totalOfHoursApproved +" horas"));
+        totalCell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        hoursTable.addCell(totalCell);
+        totalCell = new PdfPCell();
+        hoursTable.addCell(totalCell);
+        pdfReport.add(hoursTable);
+    }
+
+    private static void newBlankLine(Paragraph blankLine, int times) {
+        for(int i=0; i < times; i++){
+            blankLine.add(new Paragraph(" "));
+        }
+    }
+}
--- src/main/java/poofinal/controllers/CtrlStudentActivitiesView.java
+++ src/main/java/poofinal/controllers/CtrlStudentActivitiesView.java
@@ -20,6 +20,7 @@ import javafx.scene.input.Mnemonic;
 import javafx.stage.Stage;
 import poofinal.application.ActivitiesCellFactor;
 import poofinal.application.Management;
+import poofinal.application.PDFGenerator;
 import poofinal.entities.Activities;
 import poofinal.entities.Student;
 
@@ -74,25 +75,15 @@ public class CtrlStudentActivitiesView implements Initializable {
     public void eventButtonDone(ActionEvent actionEvent) throws IOException, DocumentException {
         //seta o estudante clicado como estudante atual
         Student student = Management.getStudentBuffer().get(Management.getKeyMatriculation());
+
         //importa todas as atividades declaradas por esse estudante nesse metodo
         List<Activities> acceptedActivities = student.getListActivities();
 
         //file path
-        Path pdfPath = Path.of("src/main/resources/files/reports/");
-        File pdfOut = new File(pdfPath +"/"+student.getName()+".pdf");
+        Path pdfPath = Path.of("out/reports/");
 
-        if(!Files.exists(pdfPath)) {
-            Files.createDirectories(pdfPath);
-        }
+        PDFGenerator.pdfSaver(pdfPath, student.getName());
 
-        //instancia documento e cria sua saida
-        Document docPdf = new Document();
-        PdfWriter.getInstance(docPdf, new FileOutputStream(pdfOut));
-
-        //abre escrita no documento
-        docPdf.open();
-        docPdf = createReportPDF(docPdf);
-        docPdf.close();
         File file = new File("src\\main\\resources\\files\\studentActivities\\" + student.getName() + "\\");
         if(file.exists() && file.isDirectory()){
             File listFiles[] = file.listFiles();
@@ -102,97 +93,6 @@ public class CtrlStudentActivitiesView implements Initializable {
         }
         file.deleteOnExit();
         Management.changeScene("successPDFGenerated.fxml", actionEvent);
-    }
-    //Daqui pra baixo criador de PDF
-    private Document createReportPDF(Document pdfReport) throws DocumentException, IOException {
-        Student student = Management.getStudentBuffer().get(Management.getKeyMatriculation());
-        List<Activities> acceptedActivities = student.getListActivities();
-
-        String isApproved;
-
-        //inicializa o tamanho do PDF e coloca um titulo no arquivo
-        pdfReport.setPageSize(PageSize.A4);
-        pdfReport.addTitle("Relatório de Admissão de Horas Complementares");
-
-        //adiciona o logo da ufpel
-        Image img = Image.getInstance("src/main/resources/files/img/UFPelShieldLogo.png");
-        img.scaleToFit(100,100);
-        pdfReport.add(img);
-
-        //adiciona o titulo no PDF
-        Font title = new Font(FontFamily.HELVETICA, 28, Font.BOLD);
-        Paragraph titleLine = new Paragraph(String.format("Relatório de Admissão%nde Horas Complementares"), title);
-
-        titleLine.setIndentationLeft(img.getScaledWidth()+pdfReport.leftMargin());
-        titleLine.setSpacingBefore(img.getScaledHeight()*(-1));
-        pdfReport.add(titleLine);
-        titleLine.clear();
-
-        //adiciona dados do aluno
-        Paragraph writingLine = new Paragraph();
-        newBlankLine(writingLine, 3);
-        Font studentData = new Font(FontFamily.HELVETICA, 12, Font.NORMAL);
-        writingLine.add(new Paragraph(String.format("Aluno:     %s%n", student.getName()), studentData));
-        writingLine.add(new Paragraph(String.format("Matrícula: %s%n", student.getMatriculation()), studentData));
-        writingLine.add(new Paragraph(String.format("E-mail:    %s%n", student.getEmail()), studentData));
-        writingLine.add(new Paragraph(String.format("Curso:     %s%n", student.getCourse().getDescription()), studentData));
-        newBlankLine(writingLine,3);
-        pdfReport.add(writingLine);
-        writingLine.clear();
-
-        //adiciona a tabela
-        newHoursTable(pdfReport, acceptedActivities);
-
-        return pdfReport;
-    }
-
-    private void newHoursTable(Document pdfReport, List<Activities> activitiesPrintable) throws DocumentException {
-        float[] columnsWidth = {200, 50, 80};
-        int totalOfHoursApproved = 0;
-        PdfPTable hoursTable = new PdfPTable(3);
-        hoursTable.setHorizontalAlignment(Element.ALIGN_CENTER);
-        hoursTable.setTotalWidth(columnsWidth);
-        PdfPCell column = new PdfPCell(new Phrase("ATIVIDADE"));
-        column.setHorizontalAlignment(Element.ALIGN_CENTER);
-        hoursTable.addCell(column);
-        column = new PdfPCell(new Phrase("HORAS"));
-        column.setHorizontalAlignment(Element.ALIGN_CENTER);
-        hoursTable.addCell(column);
-        column = new PdfPCell(new Phrase("SITUAÇÃO"));
-        column.setHorizontalAlignment(Element.ALIGN_CENTER);
-        hoursTable.addCell(column);
-        hoursTable.setHeaderRows(1);
-        for(int i = 0; i<activitiesPrintable.size(); i++){
-            Activities printingActivitie = activitiesPrintable.get(i);
-            PdfPCell hourCell = new PdfPCell(new Phrase(printingActivitie.getDescription()));
-            hourCell.setHorizontalAlignment(Element.ALIGN_LEFT);
-            hoursTable.addCell(hourCell);
-            hourCell = new PdfPCell(new Phrase(printingActivitie.getHours()+" horas"));
-            hourCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            hoursTable.addCell(hourCell);
-            if(printingActivitie.getFlag()){
-                totalOfHoursApproved += Integer.parseInt(printingActivitie.getHours());
-                hourCell = new PdfPCell(new Phrase("APROVADO"));
-            }else{
-                hourCell = new PdfPCell(new Phrase("REPROVADO"));
-            }
-            hourCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            hoursTable.addCell(hourCell);
-        }
-        PdfPCell totalCell = new PdfPCell(new Phrase("TOTAL DE HORAS ADQUIRIDAS"));
-        hoursTable.addCell(totalCell);
-        totalCell = new PdfPCell(new Phrase(totalOfHoursApproved +" horas"));
-        totalCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        hoursTable.addCell(totalCell);
-        totalCell = new PdfPCell();
-        hoursTable.addCell(totalCell);
-        pdfReport.add(hoursTable);
-    }
-
-    private void newBlankLine(Paragraph blankLine, int times) {
-        for(int i=0; i < times; i++){
-            blankLine.add(new Paragraph(" "));
-        }
     }
 }
 
