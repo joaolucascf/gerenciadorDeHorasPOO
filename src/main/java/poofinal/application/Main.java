25a45d89fa275df943a7e852576f5a702efd1491
André <aprodrigues_10@hotmail.com>
2022-11-27 14:49:07 -0300

    Avisos de campos inválidos OK!

--- src/main/java/poofinal/application/Main.java
+++ src/main/java/poofinal/application/Main.java
@@ -3,15 +3,24 @@ package poofinal.application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 //teste
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException, ClassNotFoundException {
         Scene scene = new Scene(FXMLLoader.load(getClass().getResource("mainMenu.fxml")));
-        stage.setTitle("Gerenciador de horas complementares");
+        File file = new File("src/main/resources/files/img/cobaltoIcon.png");
+        InputStream inputStream = new FileInputStream(file);
+        Image image = new Image(inputStream);
+        stage.getIcons().add(image);
+        stage.setTitle("Gerenciador de Horas Complementares");
         stage.setScene(scene);
         stage.show();
     }
